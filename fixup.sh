092182077d574c9564762483a142c4f6f5443832
Robin Nedergård <60611967+robinnedergard@users.noreply.github.com>
2023-10-20 11:13:03 +0200

    fix: stencil fixup (#53)

--- fixup.sh
+++ fixup.sh
@@ -1,10 +1,10 @@
 echo >dist/cjs/package.json '{
   "type": "commonjs",
-	"typings": "dist/cjs/index.d.ts"
+	"typings": "./index.d.ts"
 }'
 
 echo >dist/mjs/package.json '{
   "type": "module",
-	"typings": "dist/mjs/index.d.ts"
+	"typings": "./index.d.ts"
 }'
 
